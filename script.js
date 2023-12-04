3ddbfdc3bfc29bd59afd0b2dd1c9f45bca1f4a69
MengL <MengLi@MediaWorks.co.nz>
2023-12-04 16:44:05 +1300

    tidied up the code

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-// function adBlockDetector() {
-//     setTimeout(function(){
-//         document.body.innerHTML += '<div class="adsbygoogle" id="test-ad"></div>'
-//         const testAd = document.getElementById('test-ad')
-//         const testAdStyle = getComputedStyle(testAd)
+function adBlockDetector() {
+    setTimeout(function(){
+        document.body.innerHTML += '<div class="adsbygoogle" id="test-ad"></div>'  /* cspell: disable-line */
+        const testAd = document.getElementById('test-ad')
+        const testAdStyle = getComputedStyle(testAd)
 
-//         if(testAdStyle.display==='none'){
-//             console.log('ad blocker detected!')
-//         } else{
-//             console.log('No ad blocker detected!')
-//         }
+        if(testAdStyle.display==='none'){
+            console.log('adblock detected')
+            alert('Using Ad Blockers will interfere with your listening experience and may cause silence. Turn off Ad Blockers for a better listening experience. You can do this in your browser settings or check your browsers extensions.')
+        } else{
+            fetch('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
+                .then(res=> console.log('ads allowed'))
+                .catch(err=> {
+                    console.log('adblock detected')
+                    console.log(err)
+                    alert('Using Ad Blockers will interfere with your listening experience and may cause silence. Turn off Ad Blockers for a better listening experience. You can do this in your browser settings or check your browsers extensions.')
+            })
+        }
+    }, 1000)
+}
 
-//     }, 3000)
-// }
-
-// adBlockDetector()
-
-// window.addEventListener('load', function(){
-//     fetch('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
-//     .then(res=> console.log('ads allowed'))
-//     .catch(err=>console.log('adblock detected!'))
-// })
+adBlockDetector()
 
 
-    fetch('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js')
-    .then(res=> console.log('ads allowed'))
-    .catch(err=> {
-        console.log('adblock detected!')
-        alert('adblock detected!')
-    })
